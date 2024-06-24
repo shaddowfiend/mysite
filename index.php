b63542100abbe5af65b5b719c2ef5c71ea5e2008
VictorBit <tihuno@mail.ru>
2024-06-24 21:38:48 +0300

    AddStudEnd

--- index.php
+++ index.php
@@ -36,22 +36,29 @@
             ?>
         </tbody>
     </table>
-
+    <form>
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">ADD STUDENTS</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+        
+            <label for="f_name">Firts Name</label>
+            <input type="text" name="f_name" class="form-control">
+            <label for="l_name">Last Name</label>
+            <input type="text" name="l_name" class="form-control">
+            <label for="age">Age</label>
+            <input type="text" name="age" class="form-control">
+        </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-success">Save</button>
       </div>
     </div>
   </div>
