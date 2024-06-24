eccdf63d86a576dc67596bce5d9f2114b0b8071a
VictorBit <tihuno@mail.ru>
2024-06-24 21:21:09 +0300

    addStudentsButtonRepaired

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include("db.php"); ?>
         <div class="box1">
         <h2>ALL STUDENTS</h2>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">ADD STUDENTS</button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">ADD STUDENTS</button>
         </div>
     <table class = "table table-hover table-bordered table-striped">
         <thead>
@@ -20,7 +20,6 @@
                 $result = mysqli_query($connection, $query);
                 if(!$result){
                     die("query Failed".mysqli_error());
-
                 }
                 else{
                     while($row = mysqli_fetch_assoc($result)){
@@ -40,6 +39,24 @@
         </tbody>
     </table>
 
-    
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include("footer.php");
