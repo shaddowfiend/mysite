9bbd4e514851ea65ac37f2c85e8d6dd813dd77c5
VictorBit <tihuno@mail.ru>
2024-06-25 15:18:51 +0300

    UpdateDeleteBtn

--- index.php
+++ index.php
@@ -16,8 +16,7 @@
         </tr>
         </thead>
         <tbody>
-            <?php 
-
+            <?php
                 $query = "select * from `students`";
                 $result = mysqli_query($connection, $query);
                 if(!$result){
@@ -31,6 +30,8 @@
                             <td><?php echo $row['first_name']; ?></td>
                             <td><?php echo $row['last_name']; ?></td>
                             <td><?php echo $row['age']; ?></td>
+                            <td><a href="update_page_1.php?id=<?php echo $row['id']; ?>" class="btn btn-success">Update</a></td>
+                            <td><a href="delete_page.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a></td>
                         </tr>
                         <?php
                     }
